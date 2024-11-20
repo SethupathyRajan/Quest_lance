a421cd165458834310ac73c1167c8df0a919e5a1
SethupathyRajan <rajansethupathyoffl@gmail.com>
2024-11-20 10:54:03 +0530

    resolved errors

--- client/src/components/ImageOverlay.jsx
+++ client/src/components/ImageOverlay.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
 import op from '../assets/ss.jpg'; // Ensure the correct path to your image
 import '../styles/ImageOverlay.css'; // Import the CSS file
 
 function ImgOverlay() {
   return (
-    <Card className="bg-dark">
-      {/* Use the imported image here */}
-      <Card.Img
-        src={op}
-        alt="Cardimage"
-        style={{
-          width: 'auto',  // Set the width to 50% of the container
-          height: '100%', // Maintain aspect ratio
-          objectFit: 'cover' // Ensure the image covers the area
-        }}
-      />
-      <Card.ImgOverlay>
-        <Card.Title className='c1'>Quest<super>Lance</super></Card.Title>
-        <Card.Text className='cd'>
-        "Be Your Own Boss. Start Freelancing Today!"
-        </Card.Text>
-        <Card.Text></Card.Text>
-      </Card.ImgOverlay>
-    </Card>
+        <div className="custom-card-container">
+          <img
+            src={op}
+            alt="Card"
+            className="custom-card-image"
+          />
+          <div className="custom-card-overlay">
+            <img src="{logo}" alt="" className='image-over' />
+            <p className="custom-card-description">
+              "Be Your Own Boss. Start Freelancing Today!"
+            </p>
+          </div>
+        </div>
   );
 }
 
